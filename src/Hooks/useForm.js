e772f06d68651968bca2f4cc5f2d1aab64fce847
victor-luna <victor.vazguimaraes@gmail.com>
2022-07-06 11:50:24 -0300

    Criação do cadastro de login na API

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -3,8 +3,17 @@ import React from "react";
 const types = {
   email: {
     regex:
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    message: "Preencha um e-mail válido!",
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: "Preencha um email válido",
+  },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      "A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.",
+  },
+  number: {
+    regex: /^\d+$/,
+    message: "Utilize números apenas.",
   },
 };
 
@@ -15,7 +24,7 @@ const useForm = (type) => {
   function validate(value) {
     if (type === false) return true;
     if (value.length === 0) {
-      setError("Prencha um valor.");
+      setError("Preencha um valor.");
       return false;
     } else if (types[type] && !types[type].regex.test(value)) {
       setError(types[type].message);
